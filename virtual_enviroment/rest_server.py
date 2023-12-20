8dc8f6484ff0917aa6dbc6d269038190ce40e873
Clare Tubridy <G00354146@atu.ie>
2023-12-20 16:24:20 +0000

    Updated code for shop

--- virtual_enviroment/rest_server.py
+++ virtual_enviroment/rest_server.py
@@ -1,73 +1,105 @@
 from flask import Flask, url_for, request, redirect, abort, jsonify
+import mysql.connector
 
 app = Flask(__name__, static_url_path='', static_folder='staticpages')
 
-books = [
-    {"id": 1, "Title":"Harry Potter", "Author":"JK", "Price":1000},
-    {"id": 2, "Title":"The Mortal Instruments", "Author":"Cassandra Clare", "Price":2000},
-    {"id": 3, "Title":"Cooking Book", "Author":"Clare Tubridy", "Price":1500},
-]
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'root',
+    database = 'shoedata'
+)
 
-nextId = 4
+mycursor = mydb.cursor()
+sql= '''
+    CREATE TABLE IF NOT EXISTS productdata (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        Product VARCHAR(50),
+        Model VARCHAR(50),
+        Price INT
+    )
+'''
+mycursor.execute(sql)
+mydb.commit()
 
+# RESTful API Endpoints
 # Get All
-@app.route('/books')
+@app.route('/shoes', methods=['GET'])
 def getAll():
-    return jsonify(books)
+    mycursor.execute('SELECT * FROM productdata')
+    shoes = mycursor.fetchall()
+    return jsonify({'shoes': [{'id': shoes[0], 'title': shoes[1], 'description': shoes[2], 'done': shoes[3]} for shoe in shoes]})
 
 # Find by Id
-@app.route('/books/<int:id>')
+@app.route('/shoes/<int:id>')
 def findById(id):
-    foundBooks = list(filter (lambda t : t["id"] == id, books))
-    if len(foundBooks) == 0:
-        return jsonify({}) , 204
-    return jsonify(foundBooks[0])
+    mycursor.execute(f'SELECT * FROM productdata WHERE id = {id}')
+    shoe = mycursor.fetchone()
+    if not shoe:
+        return jsonify({}), 204
+    return jsonify({'shoe': {'id': shoe[0], 'Product': shoe[1], 'Model': shoe[2], 'Price': shoe[3]}})
 
 # Create
-#  curl -X POST http://127.0.0.1:5000/books
-@app.route('/books', methods=['POST'])
+#  curl -X POST http://127.0.0.1:5000/shoes
+@app.route('/shoes', methods=['POST'])
 def create():
-    global nextId
     if not request.json:
         abort(400)
 
-    book = {
-        "id": nextId, 
-        "Title":"Game Of Thrones", 
-        "Author":"George R. R. Martin", 
-        "Price":500
+    new_shoe = {
+        'Product': request.json['Product'],
+        'Model': request.json['Model'],
+        'Price': request.json['Price']
     }
 
-    books.append(book)
-    nextId += 1
-    return jsonify(book)
+    sql = 'INSERT INTO productdata (Product, Model, Price) VALUES (%s, %s, %s)'
+    values = (new_shoe['Product'], new_shoe['Model'], new_shoe['Price'])
+
+    mycursor.execute(sql, values)
+    mydb.commit()
+
+    new_shoe['id'] = mycursor.lastrowid
+    return jsonify({'shoe': new_shoe}), 201
 
 # Update
 # curl -X PUT -d "{\"Price\":999}" -H "content-type:application/json" http://127.0.0.1:5000/books/1
-@app.route('/books/<int:id>', methods=['PUT'])
+@app.route('/shoes/<int:id>', methods=['PUT'])
 def update(id):
-    foundBooks = list(filter(lambda t : t["id"] == id, books))
-    if len(foundBooks) == 0:
-        return jsonify({}), 404
-    
-    currentBook = foundBooks[0]
-    if 'Title' in request.json:
-        currentBook['Title'] = request.json['Title']
-    if 'Author' in request.json:
-        currentBook['Author'] = request.json['Author']
-    if 'Price' in request.json:
-        currentBook['Price'] = request.json['Price']
+    mycursor.execute(f'SELECT * FROM productdata WHERE id = {id}')
+    current_shoe = mycursor.fetchone()
 
-    return jsonify(currentBook)
+    if not current_shoe:
+        return jsonify({}), 404
+
+    if 'Product' in request.json:
+        current_shoe[1] = request.json['Product']
+    if 'Model' in request.json:
+        current_shoe[2] = request.json['Model']
+    if 'Price' in request.json:
+        current_shoe[3] = request.json['Price']
+
+    sql = 'UPDATE productdata SET Product=%s, Model=%s, Price=%s WHERE id=%s'
+    values = (current_shoe[1], current_shoe[2], current_shoe[3], id)
+
+    mycursor.execute(sql, values)
+    mydb.commit()
+
+    return jsonify({'shoe': {'id': current_shoe[0], 'Product': current_shoe[1], 'Model': current_shoe[2], 'Price': current_shoe[3]}})
 
 # Delete
-@app.route('/books/<int:id>', methods=['DELETE'])
+@app.route('/shoes/<int:id>', methods=['DELETE'])
 def delete(id):
-    foundBooks = list(filter(lambda t : t["id"] == id, books))
-    if len(foundBooks) == 0:
+    mycursor.execute(f'SELECT * FROM productdata WHERE id = {id}')
+    deleted_shoe = mycursor.fetchone()
+
+    if not deleted_shoe:
         return jsonify({}), 404
+
+    sql = 'DELETE FROM productdata WHERE id=%s'
+    values = (id,)
+    mycursor.execute(sql, values)
+    mydb.commit()
     
-    books.remove(foundBooks[0])
     return jsonify({"done":True})
 
 if __name__ == '__main__':
