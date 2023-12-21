605e4215750facd1043473fee0cd04b4fb6a0987
Clare Tubridy <G00354146@atu.ie>
2023-12-21 11:19:01 +0000

    All Functions Working

--- app.py
+++ app.py
@@ -1,48 +1,76 @@
-from flask import Flask, url_for, request, redirect, abort, jsonify
+from flask import Flask, url_for, request, redirect, abort, jsonify,render_template
 import mysql.connector
+import json
 
 app = Flask(__name__, static_url_path='', static_folder='staticpages')
 
-mydb = mysql.connector.connect(
-    host = 'localhost',
-    user = 'root',
-    password = 'root',
-    database = 'shoedata'
-)
+class ShopDAO:
+    def __init__(self, config_path='config.json'):
+        self.config = self.read_config(config_path)
+    
+    def read_config(self, config_path):
+        with open(config_path, 'r') as file:
+            config = json.load(file)
+        return config['Database']
 
-mycursor = mydb.cursor()
+    def get_cursor(self):
+        connection = mysql.connector.connect(
+            host=self.config['host'],
+            user=self.config['user'],
+            password=self.config['password'],
+            database=self.config['database']
+        )
+        mycursor = connection.cursor()
+        return mycursor, connection
+    
+    def close_all(self, mycursor, connection):
+        connection.close()
+        mycursor.close()
+
+shop_dao = ShopDAO()
+mycursor, connection = shop_dao.get_cursor()
 sql= '''
     CREATE TABLE IF NOT EXISTS productdata (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        Product VARCHAR(50),
-        Model VARCHAR(50),
+        ID INT AUTO_INCREMENT PRIMARY KEY,
+        Product VARCHAR(300),
+        Model VARCHAR(200),
         Price INT
     )
 '''
 mycursor.execute(sql)
-mydb.commit()
+connection.commit()
+shop_dao.close_all(mycursor, connection)
 
 # RESTful API Endpoints
 # Get All
 @app.route('/shoes', methods=['GET'])
 def getAll():
+    mycursor, connection = shop_dao.get_cursor()
     mycursor.execute('SELECT * FROM productdata')
     shoes = mycursor.fetchall()
-    return jsonify({'shoes': [{'id': shoes[0], 'title': shoes[1], 'description': shoes[2], 'done': shoes[3]} for shoe in shoes]})
+    shop_dao.close_all(mycursor, connection)
+
+    # Create a list of dictionaries for each shoe
+    shoes_list = [{'id': shoe[0], 'title': shoe[1], 'description': shoe[2], 'done': shoe[3]} for shoe in shoes]
+    return jsonify({'shoes': shoes_list})
 
 # Find by Id
 @app.route('/shoes/<int:id>')
 def findById(id):
+    mycursor, connection = shop_dao.get_cursor()
     mycursor.execute(f'SELECT * FROM productdata WHERE id = {id}')
     shoe = mycursor.fetchone()
+    shop_dao.close_all(mycursor, connection)
     if not shoe:
         return jsonify({}), 204
     return jsonify({'shoe': {'id': shoe[0], 'Product': shoe[1], 'Model': shoe[2], 'Price': shoe[3]}})
 
 # Create
-#  curl -X POST http://127.0.0.1:5000/shoes
+# curl -X POST -H "Content-Type: application/json" -d "{\"Product\": \"New Product\", \"Model\": \"New Model\", \"Price\": 123}" http://127.0.0.1:5000/shoes
 @app.route('/shoes', methods=['POST'])
 def create():
+    mycursor, connection = shop_dao.get_cursor()
+
     if not request.json:
         abort(400)
 
@@ -56,17 +84,19 @@ def create():
     values = (new_shoe['Product'], new_shoe['Model'], new_shoe['Price'])
 
     mycursor.execute(sql, values)
-    mydb.commit()
+    connection.commit()
 
     new_shoe['id'] = mycursor.lastrowid
+    shop_dao.close_all(mycursor, connection)
     return jsonify({'shoe': new_shoe}), 201
 
 # Update
-# curl -X PUT -d "{\"Price\":999}" -H "content-type:application/json" http://127.0.0.1:5000/books/1
+# curl -X PUT -H "Content-Type: application/json" -d "{\"Product\": \"Updated Product\", \"Model\": \"Updated Model\", \"Price\": 200}" http://127.0.0.1:5000/shoes/6
 @app.route('/shoes/<int:id>', methods=['PUT'])
 def update(id):
+    mycursor, connection = shop_dao.get_cursor()
     mycursor.execute(f'SELECT * FROM productdata WHERE id = {id}')
-    current_shoe = mycursor.fetchone()
+    current_shoe = list(mycursor.fetchone())
 
     if not current_shoe:
         return jsonify({}), 404
@@ -82,13 +112,16 @@ def update(id):
     values = (current_shoe[1], current_shoe[2], current_shoe[3], id)
 
     mycursor.execute(sql, values)
-    mydb.commit()
+    connection.commit()
 
+    shop_dao.close_all(mycursor, connection)
     return jsonify({'shoe': {'id': current_shoe[0], 'Product': current_shoe[1], 'Model': current_shoe[2], 'Price': current_shoe[3]}})
 
 # Delete
+# curl -X DELETE http://127.0.0.1:5000/shoes/7
 @app.route('/shoes/<int:id>', methods=['DELETE'])
 def delete(id):
+    mycursor, connection = shop_dao.get_cursor()
     mycursor.execute(f'SELECT * FROM productdata WHERE id = {id}')
     deleted_shoe = mycursor.fetchone()
 
@@ -98,8 +131,9 @@ def delete(id):
     sql = 'DELETE FROM productdata WHERE id=%s'
     values = (id,)
     mycursor.execute(sql, values)
-    mydb.commit()
+    connection.commit()
     
+    shop_dao.close_all(mycursor, connection)
     return jsonify({"done":True})
 
 if __name__ == '__main__':
